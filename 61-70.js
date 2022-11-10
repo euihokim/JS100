055c16b6661c39ef145aac79c1863554b6513ccf
euiho.kim <kim-eh@naver.com>
2022-11-10 23:27:32 +0900

    문제61 ~ 문제70

--- 61-70.js
+++ 61-70.js
@@ -268,3 +268,83 @@ console.log(`--------------------------문제66--------------------------`);
 
 let tower = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"];
 let rule = "ABD";
+let match = [];
+for (let i of tower) {
+    match.push(i.match(/A|B|D/g));
+}
+for (let i = 0; i < match.length; i++) {
+    if (match[i] === null || match[i].join("") === match[i].sort().join("")) {
+        console.log("가능");
+    } else {
+        console.log("불가능");
+    }
+}
+
+// solution
+// function solution(전체블록, 규칙){
+//     let answer = [];
+//     for (let 부분블록 of 전체블록){
+//       answer.push(블록순서체크(부분블록, 규칙));
+//     }
+//     return answer;
+//   }
+
+//   function 블록순서체크(부분블록, 규칙){
+//     let 임시변수 = 규칙.indexOf(규칙[0]);
+//     for (let 문자 of 부분블록){
+//       if (규칙.includes(문자)){
+//         if (임시변수 > 규칙.indexOf(문자)){
+//           return '불가능';
+//         }
+//         임시변수 = 규칙.indexOf(문자);
+//       }
+//     }
+//     return '가능';
+//   }
+
+//   const 전체블록 = ['ABCDEF', 'BCAD', 'ADEFQRX', 'BEDFG', 'AEBFDGCH'];
+//   const 규칙 = 'ABCD';
+
+//   console.log(solution(전체블록, 규칙));
+//   //["가능", "불가능", "가능", "가능", "불가능"]
+
+console.log(`--------------------------문제67--------------------------`);
+
+// 문제67 : 민규의 악수
+// 광장에서 모인 사람들과 악수를 하는 행사가 열렸습니다.
+// 참가자인 민규는 몇명의 사람들과 악수를 한 후 중간에 일이 생겨 집으로 갔습니다.
+
+// 이 행사에서 진행된 악수는 총 n번이라고 했을 때,
+// 민규는 몇 번의 악수를 하고 집으로 돌아갔을까요?
+// 그리고 민규를 포함한 행사 참가자는 몇 명일까요?
+
+// - 악수는 모두 1대 1로 진행이 됩니다.
+// - 민규를 제외한 모든 참가자는 자신을 제외한 참가자와 모두 한번씩 악수를 합니다.
+// - 같은 상대와 중복된 악수는 카운트 하지 않습니다.
+// - 민규를 제외한 참가자는 행사를 모두 마쳤습니다.
+
+// 예를들어 행사에서 59회의 악수가 진행되었다면 민규는 4번의 악수를 하였고 민규를 포함한 참가자는 12명이다.
+
+// 행사에서 진행된 악수 횟수(n)를 입력으로 받으면 민규의 악수 횟수와 행사 참가자 수가 출력됩니다.
+
+// 입력
+// 59
+
+// 출력
+// [4, 12] // [악수 횟수, 행사 참가자 수]
+
+let input = prompt("행사에서 진행된 악수 횟수(n)를 입력하세요.") * 1;
+
+// let input = "59" * 1
+// let multiply = 0;
+// let num = 1;
+// for(let n = 1; n < Infinity; n++) {
+//         multiply = (n * (n-1))/2
+//         if (input < multiply){
+//             break
+//         }
+//     ++num
+//     }
+
+// console.log(multiply)
+// console.log(num)
