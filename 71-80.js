8eb9065f19cfec37a1c2495906caa3ccbc12ab02
euiho.kim <kim-eh@naver.com>
2022-11-19 23:42:23 +0900

    Modify 문제73

--- 71-80.js
+++ 71-80.js
@@ -172,39 +172,80 @@ console.log(`--------------------------문제73--------------------------`);
 // 출력
 // 2
 
-const graph3 = {
-    A: ["B", "C"],
-    B: ["A", "D", "E"],
-    C: ["A", "F"],
-    D: ["B"],
-    E: ["B", "F"],
-    F: ["C", "E"],
-};
+// ㅠㅠ
+// const graph3 = {
+//     A: ["B", "C"],
+//     B: ["A", "D", "E"],
+//     C: ["A", "F"],
+//     D: ["B"],
+//     E: ["B", "F"],
+//     F: ["C", "E"],
+// };
 
-function bfsShortestRoute(graph3, dfsStartNode) {
-    let dfsVisited = [];
-    let dfsStack = [dfsStartNode];
+// function bfsShortestRoute(graph3, dfsStartNode) {
+//     let dfsVisited = [];
+//     let dfsStack = [dfsStartNode];
 
-    while (dfsStack.length !== 0) {
-        let dfsNode = dfsStack.pop();
-        if (!dfsVisited.includes(dfsNode)) {
-            dfsVisited.push(dfsNode);
-            dfsStack = [...dfsStack, ...graph3[dfsNode]];
-        }
-    }
-    return dfsVisited;
-}
+//     while (dfsStack.length !== 0) {
+//         let dfsNode = dfsStack.pop();
+//         if (!dfsVisited.includes(dfsNode)) {
+//             dfsVisited.push(dfsNode);
+//             dfsStack = [...dfsStack, ...graph3[dfsNode]];
+//         }
+//     }
+//     return dfsVisited;
+// }
 
-function dfsShortestRoute(graph3, bfsStartNode) {
-    let bfsVisited = [];
-    let bfsQueue = [bfsStartNode];
+// function dfsShortestRoute(graph3, bfsStartNode) {
+//     let bfsVisited = [];
+//     let bfsQueue = [bfsStartNode];
 
-    while (bfsQueue.length !== 0) {
-        let bfsNode = bfsQueue.shift();
-        if (!bfsStartNode.includes(bfsNode)) {
-            bfsVisited.push(bfsNode);
-            bfsQueue = [...bfsQueue, ...graph3[bfsNode]];
-        }
-    }
-    return bfsVisited;
-}
+//     while (bfsQueue.length !== 0) {
+//         let bfsNode = bfsQueue.shift();
+//         if (!bfsStartNode.includes(bfsNode)) {
+//             bfsVisited.push(bfsNode);
+//             bfsQueue = [...bfsQueue, ...graph3[bfsNode]];
+//         }
+//     }
+//     return bfsVisited;
+// }
+
+// solution
+// const graph = {'A': ['B', 'C'],
+//          'B': ['A', 'D', 'E'],
+//          'C': ['A', 'F'],
+//          'D': ['B'],
+//          'E': ['B', 'F'],
+//          'F': ['C', 'E']};
+
+// const user_input = prompt('입력해주세요').split(' ');
+// const start = user_input[0];
+// const end = user_input[1];
+
+// let queue = [start];
+// let visited = [start];
+
+// function solution(){
+//   let count = -1;
+
+//   while (queue.length !== 0){
+//     count += 1;
+
+//     let size = queue.length;
+
+//     for (let i=0; i<size; i++){let node = queue.splice(0,1);
+// 			if (node == end){
+//                 return count;
+//             }
+
+// 			for (let next_node in graph[node]) {
+//                 if (!visited.includes(graph[node][next_node])){
+//                     visited.push(graph[node][next_node]);
+//                     queue.push(graph[node][next_node]);
+
+//                 }
+//             }
+//         }
+//     }
+// }
+// console.log(solution());
