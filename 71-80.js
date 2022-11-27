29698307aac3b139b2feb674a4f4bc2ee6fec3b4
euiho.kim <kim-eh@naver.com>
2022-11-28 01:11:01 +0900

    Add 문제79

--- 71-80.js
+++ 71-80.js
@@ -659,3 +659,135 @@ console.log(`출력:`, food);
 // }
 
 // console.log(sol(n, k));
+
+console.log(`--------------------------문제79--------------------------`);
+
+// 문제79 : 순회하는 리스트
+
+// 다음의 값이 주어졌을 때
+// l = [10, 20, 25, 27, 34, 35, 39]
+
+// n번 순회를 결정합니다. 예를 들어 2번 순회하면
+// l = [35, 39, 10, 20, 25, 27, 34]
+
+// 여기서 변하기 전 원소와 변한 후 원소의 값의 차가 가장 작은 값을 출력하는 프로그램을 작성하세요.
+
+// 예를 들어 2번 순회했을 때 변하기 전의 리스트와 변한 후의 리스트의 값은 아래와 같습니다.
+
+// 순회전_리스트 = [10, 20, 25, 27, 34, 35, 39]
+// 순회후_리스트 = [35, 39, 10, 20, 25, 27, 34]
+// 리스트의_차 = [25, 19, 15, 7, 9, 8, 5]
+
+// 39와 변한 후의 34 값의 차가 5이므로 리스트의 차 중 최솟값입니다. 따라서 39와 34의 인덱스인 6과 39와 34를 출력하는 프로그램을 만들어주세요.
+
+// const l = [10, 20, 25, 27, 34, 35, 39]; //기존 입력 값
+// const n = parseInt(prompt('순회횟수는?), 10);
+
+// function rotate(inL, inN){
+
+// 		<빈칸을 채워주세요>
+// }
+
+// rotate(l, n)
+
+// 입력
+// 순회횟수는 : 2
+
+// 출력
+// index : 6
+// value : 39, 34
+
+// ---
+
+// 입력
+// 순회횟수는 : 3
+
+// 출력
+// index : 5
+// value : 35, 25
+
+// const l = [10, 20, 25, 27, 34, 35, 39];
+// const N = parseInt(prompt("순회횟수는?"), 10);
+
+// function rotate(inL, inN) {
+//     let rotateL = [];
+//     let result = [];
+//     let index;
+
+//     rotateL.push(inL);
+//     rotateL = rotateL.flat();
+
+//     for (let i = 1; i <= inN; i++) {
+//         rotateL.unshift(rotateL.pop());
+//     }
+
+//     for (let i = 0; i < l.length; i++) {
+//         result.push(Math.abs(inL[i] - rotateL[i]));
+//     }
+
+//     index = result.findIndex((x) => x === Math.min(...result));
+
+//     console.log(
+//         `입력\n순환횟수는 : ${inN}\n출력\nindex : ${index}\nvalue : ${inL[index]}, ${rotateL[index]}`
+//     );
+// }
+
+// rotate(l, N);
+
+const l = [10, 20, 25, 27, 34, 35, 39];
+const n1 = 2;
+const n2 = 3;
+
+function rotate(inL, inN) {
+    let rotateL = [];
+    let result = [];
+    let index;
+
+    rotateL.push(inL);
+    rotateL = rotateL.flat();
+
+    for (let i = 1; i <= inN; i++) {
+        rotateL.unshift(rotateL.pop());
+    }
+
+    for (let i = 0; i < l.length; i++) {
+        result.push(Math.abs(inL[i] - rotateL[i]));
+    }
+
+    index = result.findIndex((x) => x === Math.min(...result));
+
+    console.log(
+        `입력\n순환횟수는 : ${inN}\n\n출력\nindex : ${index}\nvalue : ${inL[index]}, ${rotateL[index]}`
+    );
+}
+
+rotate(l, n1);
+console.log(`\n`);
+rotate(l, n2);
+
+// solution
+// function rotate(a, t){
+//     let b = a.slice();
+//     let c = [];
+//     for (let i = 0; i < t; i++){
+//         b.unshift(b.pop());
+//     }
+
+//     for (let i in a){ // let i in b 로 해도됩니다.
+//         c.push(Math.abs(a[i]-b[i]));
+//     }
+
+//     //최솟값
+//     const m = Math.min.apply(null, c);
+
+// 		//최솟값의 인덱스 구하기
+//     let index = c.indexOf(m);
+
+//     console.log("index :", index);
+//     console.log("value :", a[index], b[index]);
+// }
+
+// const l = [10, 20, 25, 27, 34, 35, 39]; //기존 입력 값
+// const turn = prompt('순회 횟수는?');
+
+// rotate(l, turn);
